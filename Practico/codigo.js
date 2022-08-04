e200eb36316a85b15adcd15b3ef2686e7cc7fa4d
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-08-04 19:07:01 -0300

    Escribiendo HTML desde JS

--- Practico/codigo.js
+++ Practico/codigo.js
@@ -13,3 +13,27 @@ print(input)
 function print(text){
     console.log(text)
 }
+
+title.innerHTML = "Patito <br> Feo";
+//title.innerText = "Patito <br> Feo";
+
+//Get
+print(title.getAttribute("titleh1"));
+//Set
+title.setAttribute("class","Rojo")
+//Add Remove
+title.classList.add("Verde");
+//title.classList.remote("Rojo");
+
+input.value = "Digrutt"
+
+
+print(document.createElement("span"));
+//Create
+const image = document.createElement("img");
+//Add Atribute
+image.setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo9xqsZF_yYunSeg9vuq2UBWCGuuID6B7T1DCX2UQP&s")
+//Add to idParrafo
+idParrafo.append(image);
+
+print(image);
