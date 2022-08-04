115b6929ea7cc7cbb5d3d5ced59694ecde4187d2
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-08-04 18:48:50 -0300

    Leyendo HTML desde JS

--- Practico/codigo.js
+++ Practico/codigo.js
@@ -1,1 +1,15 @@
-console.log("Hello,World")
+const title = document.querySelector("h1");
+const parrafo = document.querySelector("p");
+const classParrafo = document.querySelector(".parrafo");
+const idParrafo = document.querySelector("#pid");
+const input = document.querySelector("input");
+
+print(title);
+print(parrafo);
+print(classParrafo);
+print(idParrafo);
+print(input)
+
+function print(text){
+    console.log(text)
+}
