0b62b45d45eef8b3c2777c773333f5f2b6233b68
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-08-04 23:48:56 -0300

    addEventListener

--- Practico/codigo.js
+++ Practico/codigo.js
@@ -2,11 +2,44 @@ const title = document.querySelector("h1");
 const inputValor1 = document.querySelector("#calculo1");
 const inputValor2 = document.querySelector("#calculo2");
 const btnCalcular = document.querySelector("#btnCalcular");
+const form = document.querySelector("form");
 
-console.log(inputValor1);
+
+//Ejemplo 1
+/*
+//Escucha cada vez que suceda un evento
+//No se coloca parentecis porque manda a llamar y la misma funcion addEventListener coloca los parentecis
+//Esto ayuda mejor a la legibilidad
+form.addEventListener('submit', sumarInputs);
 
 //Una de las formas de escuchar los elementos
-function btnOnClick(){
+function sumarInputs(event){
+    console.log(event);
+    //Ejecuta lo que no tiene que ejecutar como defecto, osea no recargar la pagina
+    event.preventDefault();
+
+    let valor1 = Number(inputValor1.value);
+    let valor2 = Number(inputValor2.value);
+
+    let resultado = valor1 + valor2;
+    console.log(resultado)
+
+    //Creamos el Elemento parrafo
+    const parrafo = document.createElement("p");
+    parrafo.innerText = resultado;
+
+    //Agregamos el parrafo al body
+    const body = document.querySelector("body");
+    body.append(parrafo);
+}
+*/
+
+
+//Ejemplo 2
+btnCalcular.addEventListener('click', sumarInputs);
+
+//Una de las formas de escuchar los elementos
+function sumarInputs(){
     let valor1 = Number(inputValor1.value);
     let valor2 = Number(inputValor2.value);
 
