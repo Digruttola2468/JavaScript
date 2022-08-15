71df9dd50218aa54e1781c5f6281aec2cf2df404
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-08-15 16:03:26 -0300

    -

--- Intermedio/ECMAScript/src/es8/main.js
+++ Intermedio/ECMAScript/src/es8/main.js
@@ -18,3 +18,33 @@ const string = "hello";
 console.log(string.padStart(7, "Hi"));
 console.log(string.padEnd(12," ----"));
 console.log("Food".padEnd(12,"---"));
+
+
+//----------
+//Async Await
+const helloWorld = () => {
+    return new Promise( (resolver, reject) => {
+        ( false )
+            ? setTimeout( () => resolver("Hello world"), 3000)
+            : reject(new Error("Test error"));
+    })
+}
+
+//Funcion que va a implementar de async
+const HelloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+HelloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherFunction();
