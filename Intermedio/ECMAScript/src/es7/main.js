db799e363ccafe307c428c5e015f5e3ff52e561c
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-08-15 15:44:15 -0300

    -

---
+++ Intermedio/ECMAScript/src/es7/main.js
@@ -0,0 +1,12 @@
+let numbers = [1,2,3,4,5,6,7,8];
+
+if ( numbers.includes(9) ) 
+    console.log("Contiene el numero 7");
+else
+    console.log("No se encuentra");
+
+let base = 3;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
